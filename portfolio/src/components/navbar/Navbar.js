300a228720f595a7fa5e1649a0b49727f804cc0c
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2023-10-05 16:48:04 +0000

    portfolio

---
+++ portfolio/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src="" alt="" />
+      <div className="desktopMenu">
+
+      </div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" />
+      </button>
+    </nav>
+  )
+}
+
+export default Navbar
